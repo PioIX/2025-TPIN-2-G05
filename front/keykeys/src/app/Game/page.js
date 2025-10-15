0eee3a006ce491c57beff56a0b0b397cee98a9f7
jbrianza-pio <jmbrianza@pioix.edu.ar>
2025-10-15 08:48:48 -0300

    update loop
    hecha parte de empieza partida y de ca,mbio de turno

--- front/keykeys/src/app/Game/page.js
+++ front/keykeys/src/app/Game/page.js
@@ -2,7 +2,7 @@
 
 import styles from "./page.module.css";
 import clsx from "clsx";
-// import {  } from "@/api/fetch"; //REEMPLAZAR CON EL FETCH CORRESPONDIENTE
+// import {  } from "@/API/fetch"; //REEMPLAZAR CON EL FETCH CORRESPONDIENTE
 import Input from "@/Components/Input";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
@@ -14,26 +14,54 @@ export default function Game() {
   const [prevPalabra, setPrevPalabra] = useState("");
   const [letrasprohibidas, setLetrasprohibidas] = useState([]);
   const [rondas, setRondas] = useState("");
-  const [ronda, setRonda] = useState("");
-  const [activo, setActivo] = useState("");
+  const [ronda, setRonda] = useState(undefined);
+  const [activo, setActivo] = useState(undefined);
   const router = useRouter();
   
-  //codigo en eladmin
-  //hacer tema rondas
+  //codigo en eladmin y //hacer tema rondas
   useEffect(()=>{
-    //seteos
-    if(id==localStorage(idAdmin)){
-
+    //id de persona setId(localstorage(idUser))
+    //conecta a la room localstorage(room)
+    if(id==localStorage(idAdmin)){ 
+      // setRondas(localStorage(rondas))
+      if(ronda==undefined){
+        setRonda(1)
+      }
+      setActivo(true)
     }
   },[])
-  //cada vez que te llega el turno
+    //cada vez que te llega el , evento de cambio de ronda + al inicio
   useEffect(()=>{
-    const letras = "abcdefghijklmnñopqrstuvwxyz"
-    for(let i=0;i<5;i++){
-      const indiceAleatorio = Math.floor(Math.random() * letras.length);
-      setLetrasprohibidas((prev)=>[...prev, letras.charAt(indiceAleatorio)]) ;
+    if(id==localStorage(idAdmin)){   
+      if(ronda>rondas){
+        //termina la partida
+      }else{
+        setRonda(ronda+1)
+        setLetrasprohibidas([])
+        setPrevPalabra("")
+        const letras = "abcdefghijklmnñopqrstuvwxyz"
+        for(let i=0;i<5;i++){
+          // setActivo(true) hay que hacer que el admin no juegue en la ronda inicial siempre//mensaje en socketTurno
+          const indiceAleatorio = Math.floor(Math.random() * letras.length);
+          setLetrasprohibidas((prev)=>[...prev, letras.charAt(indiceAleatorio)]) ; 
+        }
+      }
     }
-  },[socket])
+  },[ronda, socketRonda])
+  //cada vez que te llega el evento de cambio de turno
+  useEffect(()=>{
+    if(id==socket.idTurno){
+      setPrevPalabra(palabra)
+      setRonda(socket.ronda)
+      setPalabra("")
+      //setear el input a vacio
+      setLetrasprohibidas(socket.letras)
+      setActivo(true)
+    }else{
+      //seteo de pantalla de espera gris + nombre de quien tiene el turno
+      // setPlayerActive(socket.nameTurno)
+    }
+  },[socketTurno])
   //hacer tema partida
   
   //esto va en el on key down
