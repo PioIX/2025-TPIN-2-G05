ad3805ad9bd69b66d0560be43412f6a8088d6d64
jbrianza-pio <jmbrianza@pioix.edu.ar>
2025-10-21 08:45:04 -0300

    continua point

--- front/keykeys/src/Components/UserPoint.js
+++ front/keykeys/src/Components/UserPoint.js
@@ -2,7 +2,26 @@
 import styles from "@/Components/UserPoint.module.css"
 //deben tener una foto y puntos
 export default function Person(props){
-    
+    function generateArray(number) {
+    let result = [];
+    while (number >= 10) {
+        result.push(10);
+        number -= 10;
+    }
+    while (number >= 5) {
+        result.push(5);
+        number -= 5;
+    }
+    while (number >= 3) {
+        result.push(3);
+        number -= 3;
+    }
+    while (number >= 1) {
+        result.push(1);
+        number -= 1;
+    }
+    return result;
+    }
     return(
         <div className={styles.container}>
             {/* stars  */}
